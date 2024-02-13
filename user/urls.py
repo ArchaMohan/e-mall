ec6fc0e305f76b961b51eb7fc8eaf7b4b1b79e06
Archa S Mohan <archa.mohan@experionglobal.local>
2024-02-13 12:35:32 +0530

    rest framework add

---
+++ user/urls.py
@@ -0,0 +1,16 @@
+#####emall/user/urls
+
+from django.urls import path,include
+from .views import*
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
+urlpatterns = [
+    path('',index,name="index"),
+     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+]
